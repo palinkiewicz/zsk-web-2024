a6e310b05b177f3afda8e7cb4092123a022f4574
palinkiewicz <pogromca.ap@gmail.com>
2024-09-22 15:02:31 +0200

    Add get_params route

--- lab_01/index.js
+++ lab_01/index.js
@@ -1,22 +1,22 @@
 const http = require('http'),
-      fs = require('fs');
+    fs = require('fs'),
+    url = require('url');
 
 http.createServer(function (req, res) {
+    const parsedUrl = url.parse(req.url, true);
+
     res.statusCode = 200;
 
-    if (req.url === '/route1') {
+    if (parsedUrl.pathname === '/route1') {
         res.setHeader('Content-Type', 'text/plain; charset=utf-8');
         res.write('Strona główna');
-    }
-    else if (req.url === '/route2') {
+    } else if (parsedUrl.pathname === '/route2') {
         res.setHeader('Content-Type', 'application/json; charset=utf-8');
         res.write(JSON.stringify({name: 'Adam', age: 18}));
-    }
-    else if (req.url === '/route3') {
+    } else if (parsedUrl.pathname === '/route3') {
         res.setHeader('Content-Type', 'text/html; charset=utf-8');
         res.write('<h1>Hello World</h1><p>Lorem ipsum</p>');
-    }
-    else if (req.url === '/route4') {
+    } else if (parsedUrl.pathname === '/route4') {
         res.setHeader('Content-Type', 'text/html; charset=utf-8');
 
         try {
@@ -26,6 +26,10 @@ http.createServer(function (req, res) {
             res.statusCode = 500;
             console.error(e);
         }
+    } else if (parsedUrl.pathname === '/get_params') {
+        console.log(parsedUrl.query);
+        fs.writeFileSync(`./params_${Date.now()}.json`, JSON.stringify(parsedUrl.query));
+        res.write(JSON.stringify({"ok": "ok"}));
     } else {
         res.statusCode = 404;
         res.setHeader('Content-Type', 'text/plain; charset=utf-8');
