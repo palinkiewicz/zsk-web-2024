63dd390513601a1335f08db89cf399f3b26bafff
palinkiewicz <pogromca.ap@gmail.com>
2024-10-15 22:29:55 +0200

    Do lesson task

--- lab_03_express/routes/index.js
+++ lab_03_express/routes/index.js
@@ -1,10 +1,40 @@
 const express = require('express');
-const path = require("node:path");
+const path = require('node:path');
+const fs = require('fs');
 const router = express.Router();
 
-/* GET home page. */
+function sendView(viewFile, res) {
+  const layout = fs.readFileSync(path.join(__dirname, '../public/layout.html'), 'utf-8');
+  const content = fs.readFileSync(path.join(__dirname, '../public/views/' + viewFile), 'utf-8');
+
+  const page = layout.replace('<!--CONTENT-->', content);
+
+  res.send(page);
+}
+
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  sendView('home.html', res);
 });
+
+router.get('/o-nas', function(req, res, next) {
+  sendView('about.html', res);
+});
+
+router.get('/oferta', function(req, res, next) {
+  sendView('offer.html', res);
+});
+
+router.get('/kontakt', function(req, res, next) {
+  sendView('contact.html', res);
+});
+
+router.post('/kontakt-data', function(req, res, next) {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+router.get('/*', function(req, res, next) {
+  res.send('<h1>404 Not Found</h1>');
+})
 
 module.exports = router;
