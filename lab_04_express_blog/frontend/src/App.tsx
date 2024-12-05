a05c97a0d03f4e0cd885ff26fc920f89afa0f933
palinkiewicz <pogromca.ap@gmail.com>
2024-12-05 09:35:09 +0100

    Create some router stuff

--- lab_04_express_blog/frontend/src/App.tsx
+++ lab_04_express_blog/frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import { Route, Routes } from "react-router"
+import Home from './pages/Home.tsx'
+import About from './pages/About.tsx'
+import Help from './pages/Help.tsx'
+
 export default function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <Routes>
+      <Route index element={ <Home /> } />
+      <Route path="about" element={ <About /> } />
+      <Route path="help" element={ <Help /> } />
+    </Routes>
   )
 }
---
+++ lab_04_express_blog/frontend/src/pages/About.tsx
@@ -0,0 +1,6 @@
+export default function About() {
+    return <>
+        <h1>Welcome to [Your App Name]!</h1>
+        <p>At [Your App Name], we believe in simplifying [problem your app solves] for everyone. Whether you're [your target audience, e.g., "a busy professional", "a student", or "a small business owner"], our goal is to help you [core benefit your app provides, e.g., "stay organized", "connect effortlessly", or "achieve your goals"].</p>
+    </>
+}
